7e3e0478408b19fea8adedb23779988efb85d937
Adarsh <inforaj.2022@gmail.com>
2024-06-27 19:56:13 +0530

    Desgining game window along with spaceship

---
+++ README.md
@@ -0,0 +1,2 @@
+# Space- Invaders Game
+Starting files along with designing game window and spaceship.
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <raylib.h>
+#include "spaceship.hpp"
+
+
+int main()
+{
+    Color grey = {29, 29, 27, 255};
+    int windowWidth = 750;
+    int windowHeight = 700;
+
+    InitWindow(windowWidth, windowHeight, "C++ Space Invaders");
+    SetTargetFPS(60);
+
+    Spaceship spaceship;
+
+    while(WindowShouldClose() == false){
+        
+        BeginDrawing();
+        ClearBackground(grey);
+        spaceship.Draw();
+        EndDrawing();
+
+    }
+    CloseWindow();
+}
